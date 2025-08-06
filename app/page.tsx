e26cb964580d9707707a07fdb428b487ce1e22d3
Sakda Chin <lqykim275@gmail.com>
2025-08-07 02:11:54 +0700

    Add Project

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 export default function Home() {
   return (
@@ -10,8 +11,18 @@ export default function Home() {
         <header className="flex w-full container">
           <Navbar />
         </header>
-        <section className="flex flex-col gap-8 items-center justify-center text-center container mt-50">
+        <section
+          id="home"
+          className="flex flex-col gap-8 items-center justify-center text-center container mt-50"
+        >
           <Hero />
+        </section>
+        <div className="p-10"></div>
+        <section
+          id="about"
+          className="flex flex-col gap-8 items-center justify-center text-center container min-h-screen py-20 rounded-2xl"
+        >
+          <About />
         </section>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
